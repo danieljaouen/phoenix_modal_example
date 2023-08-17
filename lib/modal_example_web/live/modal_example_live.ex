a4fc12a7c18681ac9fcfe47f8344cac60632cf97
Daniel Jaouen <daniel@danieljaouendevelopment.com>
2023-08-17 16:52:41 -0400

    Get form transitions working

--- lib/modal_example_web/live/modal_example_live.ex
+++ lib/modal_example_web/live/modal_example_live.ex
@@ -5,15 +5,40 @@ defmodule ModalExampleWeb.ModalExampleLive do
     {:ok, assign(socket, modal_open: false)}
   end
 
-  def handle_event("open-modal", _, socket) do
-    {:noreply, assign(socket, modal_open: true)}
+  def handle_params(params, url, socket) do
+    if url =~ ~p"/submit" do
+      # save_form(socket.form)
+      {
+        :noreply,
+        socket
+        |> push_patch(to: ~p"/")
+      }
+    else
+      {
+        :noreply,
+        socket
+      }
+    end
   end
 
-  def handle_event("close-modal", _, socket) do
-    {:noreply, assign(socket, modal_open: false)}
+  def show_modal_1(js \\ %JS{}) do
+    js
+    |> JS.hide(transition: "fade-out", to: "#modal-2")
+    |> JS.hide(transition: "fade-out", to: "#modal-3")
+    |> JS.show(transition: "fade-in", to: "#modal-1")
   end
 
-  def submit(_params, _session, socket) do
-    {:ok, assign(socket, form_submitted: true)}
+  def show_modal_2(js \\ %JS{}) do
+    js
+    |> JS.hide(transition: "fade-out", to: "#modal-1")
+    |> JS.hide(transition: "fade-out", to: "#modal-3")
+    |> JS.show(transition: "fade-in", to: "#modal-2")
+  end
+
+  def show_modal_3(js \\ %JS{}) do
+    js
+    |> JS.hide(transition: "fade-out", to: "#modal-1")
+    |> JS.hide(transition: "fade-out", to: "#modal-2")
+    |> JS.show(transition: "fade-in", to: "#modal-3")
   end
 end
