7a800b1f95a0eae9187dff9bdf84664db92b4ced
Daniel Jaouen <daniel@danieljaouendevelopment.com>
2023-08-17 15:36:59 -0400

    Remove unused render function

--- lib/modal_example_web/live/modal_example_live.ex
+++ lib/modal_example_web/live/modal_example_live.ex
@@ -1,11 +1,6 @@
 defmodule ModalExampleWeb.ModalExampleLive do
   use ModalExampleWeb, :live_view
 
-  def render(assigns) do
-    ~L"""
-    """
-  end
-
   def mount(_params, _session, socket) do
     {:ok, assign(socket, modal_open: false)}
   end
@@ -16,5 +11,9 @@ defmodule ModalExampleWeb.ModalExampleLive do
 
   def handle_event("close-modal", _, socket) do
     {:noreply, assign(socket, modal_open: false)}
+  end
+
+  def submit(_params, _session, socket) do
+    {:ok, assign(socket, form_submitted: true)}
   end
 end
