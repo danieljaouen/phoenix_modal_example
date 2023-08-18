ee9b75d07d8f8bb3260c316ab97455f7ac6d72ee
Daniel Jaouen <daniel@danieljaouendevelopment.com>
2023-08-17 21:18:30 -0400

    Start fix of form errors

--- lib/modal_example_web/live/modal_example_live.ex
+++ lib/modal_example_web/live/modal_example_live.ex
@@ -21,20 +21,41 @@ defmodule ModalExampleWeb.ModalExampleLive do
     {:ok, assign(socket, form1: form1, form2: form2)}
   end
 
-  def handle_event("validate", params, socket) do
+  def handle_event("save", params, socket) do
     form1 = socket.assigns.form1
     form2 = socket.assigns.form2
 
     form1 =
       if Map.get(params, "email") do
-        validate_form1(form1, params)
+        validate_form1(to_form(params), params)
       else
         form1
       end
 
     form2 =
       if !Map.get(params, "email") do
-        validate_form2(form2, params)
+        validate_form2(to_form(params), params)
+      else
+        form2
+      end
+
+    {:noreply, assign(socket, form1: form1, form2: form2)}
+  end
+
+  def handle_event("validate", params, socket) do
+    form1 = socket.assigns.form1
+    form2 = socket.assigns.form2
+
+    form1 =
+      if Map.get(params, "email") do
+        validate_form1(to_form(params), params)
+      else
+        form1
+      end
+
+    form2 =
+      if !Map.get(params, "email") do
+        validate_form2(to_form(params), params)
       else
         form2
       end
@@ -85,40 +106,42 @@ defmodule ModalExampleWeb.ModalExampleLive do
     IO.inspect(form2)
   end
 
-  def check_field_is_blank(field) do
-    fn form -> form[field] == "" end
+  def check_field_isnt_blank(field) do
+    fn form -> form[field].value == "" end
   end
 
   def check_email_regex(form) do
     !String.match?(form[:email].value, ~r/@/)
   end
 
-  def validate_form1(_form1, params) do
-    params
-    |> validate()
-    |> put_error("email", "Email is required", check_field_is_blank("email"))
+  def validate_form1(form1, params) do
+    form1
+    |> validate(params)
+    |> put_error("email", "Email is required", check_field_isnt_blank("email"))
     |> put_error("email", "Email is invalid", &check_email_regex/1)
   end
 
-  def validate_form2(_form2, params) do
-    params
-    |> validate()
-    |> put_error("full_name", "Full name is required", check_field_is_blank("full_name"))
-    |> put_error("address1", "Address is required", check_field_is_blank("address1"))
-    |> put_error("city", "City is required", check_field_is_blank("city"))
-    |> put_error("state", "State is required", check_field_is_blank("state"))
-    |> put_error("zip", "Zip is required", check_field_is_blank("zip"))
+  def validate_form2(form2, params) do
+    form2
+    |> validate(params)
+    |> put_error("full_name", "Full name is required", check_field_isnt_blank("full_name"))
+    |> put_error("address1", "Address is required", check_field_isnt_blank("address1"))
+    |> put_error("city", "City is required", check_field_isnt_blank("city"))
+    |> put_error("state", "State is required", check_field_isnt_blank("state"))
+    |> put_error("zip", "Zip is required", check_field_isnt_blank("zip"))
   end
 
-  def validate(params) do
+  def validate(_form, params) do
     to_form(params)
   end
 
   def put_error(form, field, message, condition) do
+    # if the string is null, then the form is invalid
+    # if the string does not match, then the form is invalid
     if condition.(form) do
-      form
+      %Form{form | errors: form.errors ++ ["#{field}": {message, %{}}]}
     else
-      %Form{form | errors: ["#{field}": {message, %{}}]}
+      form
     end
   end
 end
